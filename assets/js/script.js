84b3ff5ef5e6c00dae932ff1c5d2fae669a7f40d
Katharina Kuhn <katykuhn@gmail.com>
2022-03-21 13:08:14 -0300

    Mi primer commit Se agrega viajes chile

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+$(function () {
+
+    $("a").click(function (event) {
+
+        if (this.hash !== "") {
+            event.preventDefault();
+
+            var gato = this.hash;
+
+            $("html, body").animate({
+                scrollTop: $(gato).offset().top
+            }, 800);
+        }
+    });
+
+
+
+   
+
+});
+
+$(document).scroll( function(e){
+    const y = $("html").scrollTop();
+    if(y > 300) $("nav").addClass("nav-black")
+    else $("nav").removeClass("nav-black")
+})
+
+
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+})
